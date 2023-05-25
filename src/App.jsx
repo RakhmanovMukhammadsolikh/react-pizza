cc42d7b59b10de160e64e422ef723cb92747e837
solihjon <rahmanovmuhammadsolih@gmail.com>
2023-05-25 17:31:54 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+
+import { Header } from './components/Header';
+import { Categories } from './components/Categories';
+import { Sort } from './components/Sort';
+import { PizzaBlock } from './components/PizzaBlock';
+import './scss/app.scss'
+
+function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch(`https://646f268609ff19b12086acd1.mockapi.io/items`)
+      .then((res) => {
+        return res.json()
+      })
+      .then((arr) => {
+        setItems(arr)
+      })
+  }, [])
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {items.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
